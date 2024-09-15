cc032c4b20b784510477676b6b1b4885ffafe76c
pemaismais <pedrodelafacul@gmail.com>
2024-09-15 17:15:23 -0300

    VotoService testes

--- src/test/java/urna/virtual/service/VotoServiceTest.java
+++ src/test/java/urna/virtual/service/VotoServiceTest.java
@@ -1,10 +1,12 @@
 package urna.virtual.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import urna.virtual.entity.*;
 import urna.virtual.repository.CandidatoRepository;
@@ -38,6 +40,36 @@ class VotoServiceTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @BeforeEach
+    void setup(){
+        Eleitor eleitorApto = new Eleitor(
+                10L, "eleitor001", "07535338984", Status.APTO, "Dev", "(45) 99830-3071", null, "email@email.com");
+        Eleitor eleitorPendente = new Eleitor(
+                11L, "eleitor001", "07535338984", Status.PENDENTE,"Dev","(45) 99830-3071",null,"email@email.com");
+        Eleitor eleitorBloqueado = new Eleitor(
+                12L, "eleitor001", "07535338984", Status.BLOQUEADO,"Dev","(45) 99830-3071",null,"email@email.com");
+
+        Candidato prefeitoAtivo = new Candidato(
+                20L, "prefeito-1", "519.963.910-20", Status.ATIVO, "22", 1, 0L);
+        Candidato vereadorAtivo = new Candidato(
+                30L, "vereador-1", "595.749.300-76", Status.ATIVO, "2222", 2, 0L);
+
+        Candidato prefeitoInativo = new Candidato(
+                21L, "prefeito-1", "519.963.910-20", Status.INATIVO, "22", 1, 0L);
+        Candidato vereadorInativo = new Candidato(
+                31L, "vereador-1", "595.749.300-76", Status.INATIVO, "2222", 2, 0L);
+
+        Mockito.when(eleitorRepository.findById(10L)).thenReturn(Optional.of(eleitorApto));
+        Mockito.when(eleitorRepository.findById(11L)).thenReturn(Optional.of(eleitorPendente));
+        Mockito.when(eleitorRepository.findById(12L)).thenReturn(Optional.of(eleitorBloqueado));
+
+        Mockito.when(candidatoRepository.findById(20L)).thenReturn(Optional.of(prefeitoAtivo));
+        Mockito.when(candidatoRepository.findById(30L)).thenReturn(Optional.of(vereadorAtivo));
+        Mockito.when(candidatoRepository.findById(21L)).thenReturn(Optional.of(prefeitoInativo));
+        Mockito.when(candidatoRepository.findById(31L)).thenReturn(Optional.of(vereadorInativo));
+
     }
 
     @DisplayName("Testar processo de votação")
@@ -80,6 +112,63 @@ class VotoServiceTest {
 
         System.out.println("Voto salvo: Prefeito = " + voto.getPrefeito().getNome() +
                 ", Vereador = " + voto.getVereador().getNome() + ", Data = " + voto.getDataHora() + ", HASH = " + voto.getHash());
+    }
+    @Test // Voto com Eleitor e Candidatos OK.
+    void verificarVoto001()   {
+        Candidato prefeito = new Candidato();
+        Candidato vereador = new Candidato();
+        prefeito.setId(20L);
+        vereador.setId(30L);
+
+        Voto voto = new Voto(0L, null, prefeito, vereador, null);
+
+        Assertions.assertDoesNotThrow(() -> votoService.verificarVoto(voto,10L) );
+    }
+    @Test // Voto com Vereador no lugar de prefeito.
+    void verificarVoto002()   {
+        Candidato prefeito = new Candidato();
+        Candidato vereador = new Candidato();
+        prefeito.setId(20L);
+        vereador.setId(30L);
+
+        Voto voto = new Voto(0L, null, vereador, prefeito, null);
+
+        Assertions.assertThrows(RuntimeException.class, () -> votoService.verificarVoto(voto, 10L));
+    }
+    @Test // Voto com Eleitor pendente e sendo bloqueado
+    void verificarVoto004()   {
+        Candidato prefeito = new Candidato();
+        Candidato vereador = new Candidato();
+        prefeito.setId(20L);
+        vereador.setId(30L);
+
+        Voto voto = new Voto(0L, null, prefeito, vereador, null);
+
+        Assertions.assertThrows(IllegalStateException.class, () -> votoService.verificarVoto(voto, 11L));
+    }
+
+    @Test // Voto com Eleitor bloqueado
+    void verificarVoto003()   {
+        Candidato prefeito = new Candidato();
+        Candidato vereador = new Candidato();
+        prefeito.setId(20L);
+        vereador.setId(30L);
+
+        Voto voto = new Voto(0L, null, prefeito, vereador, null);
+
+        Assertions.assertThrows(RuntimeException.class, () -> votoService.verificarVoto(voto, 12L));
+    }
+
+    @Test // Voto com Candidatos inativos
+    void verificarVoto005()   {
+        Candidato prefeito = new Candidato();
+        Candidato vereador = new Candidato();
+        prefeito.setId(21L);
+        vereador.setId(31L);
+
+        Voto voto = new Voto(0L, null, prefeito, vereador, null);
+
+        Assertions.assertThrows(RuntimeException.class, () -> votoService.verificarVoto(voto, 10L));
     }
 
 
